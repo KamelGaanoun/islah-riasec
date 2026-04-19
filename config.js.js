7cfb16b25cae5f4bf569c41cc9cb68dc934d4ec1
KamelGaanoun <48834470+KamelGaanoun@users.noreply.github.com>
2026-04-19 16:40:47 +0100

    fixed config.js

---
+++ config.js.js
@@ -0,0 +1,19 @@
+// ============================================================
+//  FICHIER MODÈLE — Copiez ce fichier en "config.js"
+//  et remplacez les valeurs par vos clés Firebase
+//  NE PAS mettre config.js sur GitHub (voir .gitignore)
+// ============================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyj-JhemTCJRTDje0h1Z71En4rM0XSPsw",
+  authDomain: "islah-riasec.firebaseapp.com",
+  databaseURL: "https://islah-riasec-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "islah-riasec",
+  storageBucket: "islah-riasec.firebasestorage.app",
+  messagingSenderId: "212531888300",
+  appId: "1:212531888300:web:004816ab69fa7c921b5f09"
+};
+
+initFirebase(firebaseConfig);
+window._routeCalled = true;
+route();
